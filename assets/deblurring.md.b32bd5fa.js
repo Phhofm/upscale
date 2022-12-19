3070088c65095bf3022c37fa4f52870b8daca0f1
Phhofm <Phhofm@users.noreply.github.com>
2022-12-19 15:44:00 +0000

    deploy: 8b112c5413c0cc4d7ac4645b76210c124b0d0724

---
+++ assets/deblurring.md.b32bd5fa.js
@@ -0,0 +1,24 @@
+import{d as s,o,c as i,b as t,a as l,e,f as a,r as n}from"./app.fd4c7dea.js";const u="/upscale/assets/deblur_example.03ca624e.jpg",p=l('<h1 id="deblurring" tabindex="-1">Deblurring <a class="header-anchor" href="#deblurring" aria-hidden="true">#</a></h1><p>Deblurring is the process of removing blur from an image as shown in this example:</p><p><img src="'+u+`" alt="Example"></p><h3 id="details" tabindex="-1">Details <a class="header-anchor" href="#details" aria-hidden="true">#</a></h3><p>Creation Date: 24. Nov 2022<br> Last Update: 25. Nov 2022<br> Models used: 23 Set Name: &#39;Deblur&#39;</p><details><summary>Models List</summary><pre><code>  1x-Focus  
+  1x-Focus_Moderate  
+  1x_ArtClarity  
+  1x_ArtClarity_strong  
+  1x_DeBLR  
+  1x_Fatality_DeBlur_270000_G  
+  1x_mdeblur  
+  1x_PixelSharpen_100000  
+  1x_ReFocus_Cleanly_100000_G  
+  1x_ReFocus_V3_140000_G  
+  1x_ThePi7on-Solidd_Deborutify_UltraLite_260k_G  
+  1x_UnResize_V3_200000_G  
+  IFAN  
+  Restormer_Defocus_Deblur  
+  Restormer_Motion_Deblurring  
+  MAXIM_GoPro  
+  MAXIM_RealBlur_J  
+  MAXIM_RealBlur_R  
+  MAXIM_REDS  
+  NAFNet_GoPro  
+  NAFNet_REDS  
+  DeblurGANv2_MobileNet  
+  DeblurGANv2_InceptionResNet-v2  
+</code></pre></details><h2 id="gopro" tabindex="-1">GoPro <a class="header-anchor" href="#gopro" aria-hidden="true">#</a></h2><p>These three example images are taken from the <a href="https://seungjunnah.github.io/Datasets/gopro" target="_blank" rel="noreferrer">GoPro Large Testset</a>. GOPRO_Large dataset is proposed for dynamic scene deblurring. Training and Test set are publicly available.</p><h3 id="example-1" tabindex="-1">Example 1 <a class="header-anchor" href="#example-1" aria-hidden="true">#</a></h3><br>`,10),d=l('<br><details><summary>Details</summary><p><p>Creation Date: 24. Nov 2022 Last Update: 25. Nov 2022<br> Image Dimensions: 1280x720 pixels<br> Type: Frame<br> Image Files: <a href="https://github.com/Phhofm/upscale/tree/sources/deblur/GOPRO_Large/test/GOPR0384_11_00" target="_blank" rel="noreferrer">Github Repo</a></p></p></details><h3 id="example-2" tabindex="-1">Example 2 <a class="header-anchor" href="#example-2" aria-hidden="true">#</a></h3><br>',4),_=l('<br><details><summary>Details</summary><p><p>Creation Date: 24. Nov 2022<br> Last Update: 25. Nov 2022<br> Image Dimensions: 1280x720 pixels<br> Type: Frame<br> Image Files: <a href="https://github.com/Phhofm/upscale/tree/sources/deblur/GOPRO_Large/test/GOPR0862_11_00" target="_blank" rel="noreferrer">Github Repo</a></p></p></details><h3 id="example-3" tabindex="-1">Example 3 <a class="header-anchor" href="#example-3" aria-hidden="true">#</a></h3><br>',4),m=e("br",null,null,-1),h=e("details",null,[e("summary",null,"Details"),e("p",null,[e("p",null,[a("Creation Date: 24. Nov 2022"),e("br"),a(" Last Update: 25. Nov 2022"),e("br"),a(" Image Dimensions: 1280x720 pixels"),e("br"),a(" Type: Frame"),e("br"),a(" Image Files: "),e("a",{href:"https://github.com/Phhofm/upscale/tree/sources/deblur/GOPRO_Large/test/GOPR0881_11_01",target:"_blank",rel:"noreferrer"},"Github Repo")])])],-1),P=JSON.parse('{"title":"Deblurring","description":"Visual Comparison of deblurring models","frontmatter":{"title":"Deblurring","description":"Visual Comparison of deblurring models","layout":"doc"},"headers":[{"level":3,"title":"Details","slug":"details","link":"#details","children":[]},{"level":2,"title":"GoPro","slug":"gopro","link":"#gopro","children":[{"level":3,"title":"Example 1","slug":"example-1","link":"#example-1","children":[]},{"level":3,"title":"Example 2","slug":"example-2","link":"#example-2","children":[]},{"level":3,"title":"Example 3","slug":"example-3","link":"#example-3","children":[]}]}],"relativePath":"deblurring.md"}'),c={name:"deblurring.md"},f=s({...c,setup(b){return(g,x)=>{const r=n("ImageSliderLocal");return o(),i("div",null,[p,t(r,{inputImageURL:"https://raw.githubusercontent.com/Phhofm/upscale/main/sources/deblur/GOPRO_Large/test/GOPR0384_11_00/000001.jpg",relativePathOutputFolder:"deblur/GOPRO_Large/test/GOPR0384_11_00/output"}),d,t(r,{inputImageURL:"https://raw.githubusercontent.com/Phhofm/upscale/main/sources/deblur/GOPRO_Large/test/GOPR0862_11_00/000004.jpg",relativePathOutputFolder:"deblur/GOPRO_Large/test/GOPR0862_11_00/output"}),_,t(r,{inputImageURL:"https://raw.githubusercontent.com/Phhofm/upscale/main/sources/deblur/GOPRO_Large/test/GOPR0881_11_01/000202.jpg",relativePathOutputFolder:"deblur/GOPRO_Large/test/GOPR0881_11_01/output"}),m,h])}}});export{P as __pageData,f as default};
